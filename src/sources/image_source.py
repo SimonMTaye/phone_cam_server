c0b89fa0b6de258b529987f27859a4b3d75723b1
SirPotato10000 <mulat.simon@gmail.com>
2021-04-07 11:15:38 +0300

    Minor changes
    Blank frame now created at run time not creation time.
    Corrected imports and removed print statements.

--- src/sources/image_source.py
+++ src/sources/image_source.py
@@ -4,18 +4,17 @@ from os import path
 from numpy import ndarray, asarray_chkfinite, zeros, uint8
 from PIL import Image
 
-from .source import Source
+from .source import AbstractSource
 
-class ImageSource(Source):
-    def __init__(self, *, width: int, height: int, fps: int) -> None:
-        self._image_data: ndarray = zeros((width, height, 4), uint8)
-        super().__init__(width=width, height=height, fps=fps)
-
+#To be Deleted
+class ImageFileSource(AbstractSource):
     def get_frame(self) -> ndarray:
+        if self._image_data is None:
+            self._image_data = zeros((self.width, self.height, 4), uint8)
         return self._image_data
 
     @classmethod
-    def create(cls, image_path: Optional[str] = None) -> "ImageSource":
+    def create(cls, image_path: Optional[str] = None) -> "ImageFileSource":
         if image_path:
             if not cls.valid_image(image_path):
                 raise ValueError("Image path is invalid")
@@ -23,7 +22,6 @@ class ImageSource(Source):
             source = cls(width=img.width, height=img.height, fps=24)
             if not source._set_image_data(asarray_chkfinite(img, dtype=uint8)):
                 raise ValueError("Error using image")
-            print(source.get_frame()[0][0])
         else:
             source = cls(width=1920, height=1080, fps=24)
         return source
