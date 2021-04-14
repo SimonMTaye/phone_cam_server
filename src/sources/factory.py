9cd504a0a47757692d25c265b46f8be3e37012ff
SirPotato10000 <mulat.simon@gmail.com>
2021-04-14 13:04:05 +0300

    Created factory interface for Sources
    Allows Sources to be used uniformly.
    Sources can have custom requirements and initilization behaviors.
    Provides interface for determining a Sources creation params at runtime.
    Separate class means only Sources that need it must use it

---
+++ src/sources/factory.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from typing import Dict 
+
+from sources.source_abstract import AbstractSource
+from collections import namedtuple
+
+ParamInfo = namedtuple("Param", ["type", "description", "required"])
+
+# For sources that need a while to initialize before being used
+
+# Used as a mixin class
+class FactoryMixin(ABC):
+
+    @staticmethod
+    @abstractmethod
+    def create(**kwargs) -> AbstractSource:
+        return NotImplementedError()
+
+    @staticmethod
+    @abstractmethod
+    def parameters() -> Dict[str, ParamInfo]:
+        return NotImplementedError()
+
+
+        
+    
+        
